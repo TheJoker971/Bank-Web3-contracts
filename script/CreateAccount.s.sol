58653d62bc8d269b0ac71bb4f0dbfd30885d4dd9
Joker <joker@mac.home>
2025-11-13 14:10:35 +0100

    Format

--- script/CreateAccount.s.sol
+++ script/CreateAccount.s.sol
@@ -1,28 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {Bank} from "../src/Bank.sol";
 import {IAccount} from "../src/interfaces/IAccount.sol";
 import {Euro} from "../src/Euro.sol";
 import {Share} from "../src/Share.sol";
 
-contract BankScript is Script {
+contract CreateAccountScript is Script {
     function run() external {
         vm.startBroadcast();
 
-        Bank bank = new Bank("My Bank");
+        Bank bank = Bank(0x5FbDB2315678afecb367f032d93F642f64180aa3);
 
         // Create an account
         address accountAddress = bank.createAccount("John", "Doe", 123456);
-        IAccount account = IAccount(accountAddress);
+        console.log("Account created at:", accountAddress);
 
-        // Check Euro balance
-        Euro euro = bank.euroToken();
-        uint256 balance = euro.balanceOf(accountAddress);
-
-        // Create a share
-        bank.createShare("My Share", "MSH", 10000 ether, 1 ether);
+        assert(bank.getBalanceOfAccount(accountAddress) == 1000 ether);
 
         vm.stopBroadcast();
     }
