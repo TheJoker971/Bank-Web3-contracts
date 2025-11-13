e2299a4049fb293dd651c53fe23e34da868c872d
Joker <joker@mac.home>
2025-11-13 02:42:07 +0100

    Format

--- src/interfaces/IAccount.sol
+++ src/interfaces/IAccount.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4; 
+pragma solidity ^0.8.4;
 
 interface IAccount {
     function transfer(address to, uint256 amount) external returns (bool);
