27a50064b1d11d3bbcb7402468131eb08568e107
Joker <joker@mac.home>
2025-11-15 16:34:43 +0100

    Adding showTotalReward

--- src/Bank.sol
+++ src/Bank.sol
@@ -256,6 +256,11 @@ contract Bank is Ownable {
         return staking.withdrawReward(to);
     }
 
+    function showStakingReward(string memory _name,address _to) external view onlyOwner returns(uint256) {
+        require(address(stakings[_name]) != address(0), StakingDoesNotExist(_name));
+        return stakings[_name].showTotalReward(_to);
+    }
+
     /**
      * @dev Approves the bank to spend euros on behalf of an account.
      * @param _from The address of the account.
